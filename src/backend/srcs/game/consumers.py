6b0fd070e13eac5d64aaddc7c104ab63e757969c
houaslam <miraihajar16@gmail.com>
2024-08-05 10:50:58 +0100

    gamePart asgi

--- src/backend/srcs/game/consumers.py
+++ src/backend/srcs/game/consumers.py
@@ -4,6 +4,10 @@ from asgiref.sync import async_to_sync
 class GameConsumer(WebsocketConsumer):
 
 	def connect(self):
+		
+
+
+
 		self.game_name = 'test';
   
 		async_to_sync(self.channel_layer.group_add)(
@@ -11,9 +15,7 @@ class GameConsumer(WebsocketConsumer):
 			self.channel_name
 		)
 		self.accept()
-		self.send(text_data=json.dumps({
-            'message': 'hello'
-        }))
+
  
 	def receive(self, text_data):
 		dataJson = json.loads(text_data)
@@ -25,11 +27,9 @@ class GameConsumer(WebsocketConsumer):
     			'data': data
        		}
 		)
-		print("MESSAGE", data)
 	
 	def create_msg(self, event):
 		data = event['data']
-		print("DATA: ", data)
 		self.send(text_data=json.dumps({
 			'type': 'msg',
             'data': data
@@ -37,5 +37,4 @@ class GameConsumer(WebsocketConsumer):
  
 
 	def disconnect(self, close_code):
-		print("BYE BYE", close_code);
 		pass
--- src/backend/srcs/game/models.py
+++ src/backend/srcs/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    oponnent = models.ForeignKey(Player, on_delete=models.CASCADE)
+    players_nb = models.IntegerField(default=0);
