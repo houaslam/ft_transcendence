d6eac4bd92b2f80519048f63639680beed038e3b
houaslam <miraihajar16@gmail.com>
2024-09-28 12:37:22 +0100

    gamePart asgi

--- src/game/backend/game/consumers.py
+++ src/game/backend/game/consumers.py
@@ -11,6 +11,7 @@ players=[]
 class GameConsumer(AsyncWebsocketConsumer):
 	gameOption = {}
 	async def connect(self):
+		print("NEW CONNECTION")
 		await self.accept()
 		await self.channel_layer.group_add("invite",self.channel_name)
 
@@ -18,34 +19,42 @@ class GameConsumer(AsyncWebsocketConsumer):
 		players.append(self)
 
 
-		self.game, self.is_host = await self.find_or_create_game()
-		self.player = await sync_to_async(models.Player.objects.create)(game=self.game, is_host=self.is_host, name='test')
-
-		self.game.player_count += 1
-		await sync_to_async(self.game.save)()
-  
+		self.is_host = await self.is_host()
 		if (self.is_host):
+			print("IS HOST")
 			await self.send(text_data=json.dumps({
 				'type' : 'gameInfo',
 				'data' : 'game_options'
 			}))
-
-		if (await self.game_is_ready(self.game) and len(players)  >= 2):
-				self.game.gameStatus = 'STARTED'
-				await sync_to_async(self.game.save)()
-				await self.start_game()
+   
+		else:
+			print("IS INVITED")
+			self.game = await self.find_game()
+			self.game.player_count += 1
+			await sync_to_async(self.game.save)()
+			if (self.game and await self.game_is_ready(self.game) and len(players)  >= 2):
+					self.game.gameStatus = 'STARTED'
+					await sync_to_async(self.game.save)()
+					await self.start_game()
 
 	@database_sync_to_async
 	def game_is_ready(self, game):
 		return game.player_count == 2
 
 	@database_sync_to_async
-	def find_or_create_game(self):
+	def is_host(self):
 		game = models.Game.objects.filter(gameStatus='WAITING').first()
 		if not game :
-			game = models.Game.objects.create(gameStatus='WAITING')
-			return game , True
-		return game, False
+			return  True
+		return  False
+
+	@database_sync_to_async
+	def create_game(self):
+		return models.Game.objects.create(gameStatus='WAITING', player_count=1)
+
+	@database_sync_to_async
+	def find_game(self):
+		return models.Game.objects.filter(gameStatus='WAITING').first()
 
 	async def start_game(self):
 		invited = players.pop()
@@ -58,10 +67,10 @@ class GameConsumer(AsyncWebsocketConsumer):
 		if (dataType == 'keycode'):
 			self.keycode = dataJson['data']
 		elif (dataType == 'gameSettings' and self.is_host):
+			self.game = await self.create_game()
 			self.game.settings = dataJson['data']
 			await sync_to_async(self.game.save)()
    
-
 	async def coordinates(self, event):
 		data = event['data']
 		await self.send(text_data=json.dumps({
